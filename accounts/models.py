0cc23ac478041870c0894d4354a56bccd65ec5d3
yi ching kang <frankyi7878@gmail.com>
2019-10-06 12:19:52 +0800

    add profile models
    add profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    avatar = models.ImageField()
