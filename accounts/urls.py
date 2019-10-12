c29ce1a17966a5205b718d77beb6f81b6ca5a7f0
yi ching kang <frankyi7878@gmail.com>
2019-10-12 11:25:31 +0800

    correct the url of edit page
    correct the url of edit page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'sign_up/$', views.sign_up, name='sign_up'),
     url(r'sign_out/$', views.sign_out, name='sign_out'),
     url(r'detail/$', views.profile_detail, name='profile_detail'),
-    url(r'edit/$', views.profile_edit, name='profile_edit'),
+    url(r'profile/edit/$', views.profile_edit, name='profile_edit'),
     url(r'change_password/$', views.change_password, name='change_password'),
 ]
