fe592841e5a2ccc355a6997e857098eff5f64688
yi ching kang <frankyi7878@gmail.com>
2019-10-14 21:48:56 +0800

    validate date and email input
    validate date and email input

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . import models
+import re
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -12,3 +13,18 @@ class ProfileForm(forms.ModelForm):
             'bio',
             'avatar',
         ]
+    def clean_date_of_birth(self):
+        date_of_birth = str(self.cleaned_data.get('date_of_birth'))
+        if re.match(r'\d{4}-\d{2}-\d{2}',date_of_birth) != None or \
+            re.match(r'\d{2}\/\d{2}\/\d{4}',date_of_birth) !=None or \
+            re.match(r'\d{2}\/\d{2}\/\d{2}',date_of_birth) !=None:
+            return date_of_birth
+        raise forms.ValidationError("""
+            date format should be YYYY-MM-DD, MM/DD/YYYY, or MM/DD/Y """)
+    def clean_email(self):
+        email = str(self.cleaned_data.get('email'))
+        if re.match(r'[-\w\d+.]+@[-\w\d.]+', email) != None:
+            return email
+        raise forms.ValidationError("""
+            email is not valida
+            """)
